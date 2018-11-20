b5e8f3e7d8c6f699905f9cdf008cef2eb11963c2
Barbara Fick <bkantzos@yahoo.com>
2018-11-19 19:58:53 -0700

    timer for each question

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -7,24 +7,26 @@ var DEBUG = false;
 // A single quiz TIMER for the moment
 // @todo replace this with a timer for each question 
 // ==============================================================================
-var interval;
+var interval;   // game timer
+var qinterval;  // question timer
 
 
 // ==============================================================================
 // The GAME OBJECT 
 // Holds view Variables
 // Holds game Variables
-// Holds game object with array of questions and answers
+// Holds Quiz - details and array of questions and answers
 // Holds game functions for game flow
 // ==============================================================================
 var game = {
 
-    // ==============================================================================
+    // ==========================================================================
     // HTML ELEMENTS - VIEW VARIABLES
-    // ==============================================================================
+    // ==========================================================================
     playBtnElem: $("#start-btn"),
     msgElem: $("#view-message"),
     timerElem: $("#view-timer"),
+    QtimerElem: $("#view-qtimer"),
     scoreElem: $("#view-score"),
     questionElem: $("#view-question"),
     imageElem: $("#view-image"),
@@ -32,13 +34,14 @@ var game = {
 
 
     time: 121,          // Start the game with a 2 minute timer
+    qtime: 10,
     score: 0,           // We start the game with a score of 0.
     currentQuestion: '',
-    questionIndex: 0,   // Variable to hold the index of current question.
 
-    // ==============================================================================
+
+    // ===========================================================================
     // QUESTIONS
-    // ==============================================================================
+    // ===========================================================================
     quiz: {
         "name": "Landmark Quiz",
         "description": "Do you know these famous landmarks?",
@@ -61,28 +64,28 @@ var game = {
     // GAME OBJECT - FUNCTIONS
     // ==============================================================================
     newGame: function () {
+
+        this.msgElem.html(this.quiz.name);
         // remove the start button from view
         // prevent new intervals from being created 
         // and existing intervals from becoming orphaned
         this.playBtnElem.hide();
         //empty all elements so that we can start a new game
         //reset all the this.quiz.questions.asked flags
-        this.quiz.questions.forEach(function(question) {
+        this.quiz.questions.forEach(function (question) {
             question.asked = false;
         });
         this.score = 0;
         this.updateScore();
-        //reset timer
+
         //@todo make a timer for each question
+        this.resetQTimer();
+
+        //reset Game timer
         clearInterval(interval);
         game.time = 121;
         // set up an interval that counts down every second
         interval = setInterval(game.countDown, 1000);
-    },
-
-    //DEBUGGING FUNCTION 
-    sayHello: function () {
-        console.log("Say Hello");
     },
 
     // ==============================================================================
@@ -92,9 +95,7 @@ var game = {
     // with the game object name
     countDown: function () {
         //if (DEBUG) console.log("countdown: " + game.time);
-        // decrease time by 1
         game.time--;
-        // update the time displayed
         game.timerElem.text("Time:" + game.time);
         // the game is over if the timer has reached 0
         if (game.time <= 0) {
@@ -103,34 +104,50 @@ var game = {
         }
     },
 
+    // Countdown the Question Timer set in game.qtime
+    // ==============================================================================
+    qcountDown: function () {
+
+        var formattedNumber = ("0" + game.qtime).slice(-2);
+
+        game.QtimerElem.text("Time: " + formattedNumber);
+        game.qtime--;
+        //if the questions time is up 
+        if (game.qtime <= -1) {
+            game.checkAnswer();
+        }
+    },
+
+    // Reset the Question Timer in a function
+    // ==============================================================================
+    resetQTimer: function () {
+        game.qtime = 10;
+        game.QtimerElem.text("Time: " + game.qtime);
+        clearInterval(qinterval);
+        qinterval = setInterval(game.qcountDown, 1000);
+    },
+
     // MAIN PROCESS
     // ==============================================================================
     play: function () {
-        //=================================================//
         this.newGame();
         this.qCycle();
     },
-
 
     // ==============================================================================
     qCycle: function () {
 
         //get a new question
         this.currentQuestion = this.chooseQuestion();
-
+        //if there are no questions left 
         if (this.currentQuestion) {
 
             if (DEBUG) console.log("CURRENT QUESTION: ", this.currentQuestion);
 
-            //ask the new question
             this.askQuestion();
-            //switch the question asked flag from false to true;
             this.currentQuestion.asked = true;
-            //update the score
             this.updateScore();
         } else {
-            //@todo this is not working
-            if (DEBUG) console.log(" typeof is undefined");
             this.gameOver();
         }
 
@@ -141,18 +158,15 @@ var game = {
     // ==============================================================================
     chooseQuestion: function () {
         //must check to see if question has already been asked and prevent that
-        //var q = this.quiz.questions[Math.floor(Math.random() * this.quiz.questions.length)];
-
         var questions = this.quiz.questions.filter(function (question) {
             return question.asked === false;
         })
-        //if (DEBUG) 
-        console.log("QUESTIONS THAT HAVE NOT BEEN ASKED: " + questions.length);
+
+        if (DEBUG) console.log("QUESTIONS THAT HAVE NOT BEEN ASKED: " + questions.length);
+
         if (questions.length > 0) {
-            var randomIndex = Math.floor((questions.length) * Math.random());
-            return questions[randomIndex];
+            return questions[Math.floor((questions.length) * Math.random())];
         } else {
-            console.log("Game should be over");
             //no unasked questions left
             return false;
         }
@@ -163,48 +177,54 @@ var game = {
     askQuestion: function () {
 
         if (DEBUG) console.log("Inside askQuestion");
+        if (DEBUG) console.log(this.currentQuestion);
 
         var elemContent = this.quiz.question + "?";
         this.display(this.questionElem.empty(), elemContent);
-        console.log(this.currentQuestion);
-        if (DEBUG) console.log(this.currentQuestion.src);
-
         this.displayImg(this.imageElem, this.currentQuestion.src, "quizImg");
-        var answer = this.currentQuestion.answer;
 
         // ====================================================
 
         var options = []; //an array of 3 possible answers
 
-        var option1 = this.chooseAnswerOption(options);
+        var option1 = this.createAnswerOption(options);
         options.push(option1["answer"]);
-        if (DEBUG) console.log("ANSWER 1: " + option1["answer"]);
-        var option2 = this.chooseAnswerOption(options);
+
+        var option2 = this.createAnswerOption(options);
         options.push(option2["answer"]);
-        if (DEBUG) console.log("ANSWER 2: " + option2["answer"]);
-        if (DEBUG) console.log(options);
 
         //Add the actual answer to a random spot in the array
-        options.splice(Math.floor((Math.random() * options.length + 1)), 0, answer);
+        options.splice(Math.floor((Math.random() * options.length + 1)), 0, this.currentQuestion.answer);
 
+        if (DEBUG) console.log("OPTIONS", options);
 
-        if (DEBUG) console.log("OPTIONS");
-        if (DEBUG) console.log(options);
-        $("#view-answers").empty();
-        options.forEach(function (name) {
+        this.answerElem.empty();
+        //using bind to get access to this.answerElem works, but seems crazy
+
+        options.forEach( function (name) {
             var button = document.createElement("button");
             button.value = name;
             button.textContent = name;
-            //@todo why isn't the this.answerElem working?
+            this.answerElem.append(button);
+        }.bind(this));
 
-            $("#view-answers").append(button);
-        });
+        // this.answerElem is available in the for loop
+        // so no closure
+        // for (var i = 0; i < options.length; i++) {
+        //     var button = document.createElement("button");
+        //     button.value = options[i];
+        //     button.textContent = options[i];
+        //     this.answerElem.append(button);
+        // }
+
 
     },
 
-    chooseAnswerOption: function (options) {
+
+    createAnswerOption: function (options) {
 
         var option = this.quiz.questions[Math.floor(Math.random() * this.quiz.questions.length)];
+
         if (DEBUG) console.log("this.currentQuestion.answer " + this.currentQuestion.answer);
         if (DEBUG) console.log("option.answer " + option.answer);
         if (DEBUG) console.log("options.indexOf(options['answer'] " + options.indexOf(options["answer"]));
@@ -212,7 +232,7 @@ var game = {
         //if the selected choice is the same as the answer try again
         //or if the selected choice is already in the list of answer choices, try again
         if (option.answer === this.currentQuestion.answer || options.indexOf(option["answer"]) !== -1) {
-            return this.chooseAnswerOption(options);
+            return this.createAnswerOption(options);
         }
 
         return option;
@@ -220,30 +240,33 @@ var game = {
 
     checkAnswer: function () {
 
-        var userAnswer = event.target.value;
-        if (DEBUG) console.log("checkAnswer: " + userAnswer);
-        if (DEBUG) console.log(this.currentQuestion.answer);
-        if (userAnswer === game.currentQuestion.answer) {
-            this.msgElem.text("Correct!", "correct");
-            this.score++;
-            this.updateScore();
+        this.resetQTimer();
+        //if time ran up then there is not a user answer        
+        if (event) {
+            if (event.target.value === game.currentQuestion.answer) {
+                this.msgElem.text("Correct!", "correct");
+                this.score++;
+                this.updateScore();
+            } else {
+                this.msgElem.text("Wrong!", "wrong");
+            }
+            if (DEBUG) console.log("checkAnswer() userAnswer " + event.target.value);
+            if (DEBUG) console.log("checkAnswer() this.currentQuestion.answer " + this.currentQuestion.answer);
         } else {
-            this.msgElem.text("Wrong!", "wrong");
+            this.msgElem.text("No more Time", "wrong");
         }
-
         this.qCycle();
     },
 
     displayImg: function (elem, elemContent, elemClass) {
-        if (DEBUG) {
-            console.log("displayImg: ");
-            console.log(elem);
-            console.log(elemContent);
-        }
         elem.empty();
         elem.html("<img class='" + elemClass + "' src='" + elemContent + "'>");
+        //@todo why doesn't this work?
+        //if (elemClass) elem.addClass(elemClass);
     },
 
+    // ==============================================================================
+    // @todo Do I really need this?
     display: function (elem, elemContent, elemClass) {
         elem.html = "";
         var pElem = document.createElement("p");
@@ -258,7 +281,7 @@ var game = {
 
     // Function that updates the score...
     updateScore: function () {
-        this.scoreElem.html("Score: " + this.score);
+        this.scoreElem.html("Score: " + this.score + "/" + this.quiz.questions.length);
     },
 
     // ==============================================================================
@@ -269,31 +292,30 @@ var game = {
     gameOver: function () {
 
         if (DEBUG) console.log("gameOver() called");
-        // clear the display
-        // remove the event listener from the option buttons
-        // stop the countdown interval
+
+        // @todo clear the display
+        // and display some game over image
+        if (this.score === 10) {
+            answerElem.empty();
+        }
         clearInterval(interval);
-        // display the score
+        clearInterval(qinterval);
+
         // display the start button so player can play again
         this.playBtnElem.show();
     }
 }
+
+
 
 // ==============================================================================
 // EVENT LISTENERS FOR 
 //      Starting the Game 
 //      Selecting an Answer
 // ==============================================================================
-$("#start-btn").on("click", function () {
-    game.play();
-});
+$("#start-btn").on("click", function () { game.play(); });
+$("#view-answers").on("click", function () { game.checkAnswer(); });
 
-$("#view-answers").on("click", function () {
-    if (DEBUG) game.sayHello();
-    // this is called every second and decreases the time
-    game.checkAnswer();
-});
-//game.newGame();
 
 
 
